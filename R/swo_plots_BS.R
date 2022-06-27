61d3bea8d08af00904707b1ca0005e0303cdb6a1
Jason Conner <Jason.Conner@noaa.gov>
2022-06-27 10:46:26 -0700

    Example of base boxplots made into overlays directly on test cases.

--- R/swo_plots_BS.R
+++ R/swo_plots_BS.R
@@ -38,7 +38,7 @@ ggplot2::theme_set(
 region = 'BS'
 
 # data ----
-spec <- vroom::vroom(here::here('data', 'species_bs.csv'))
+spec <- vroom::vroom(here::here('data', 'species_code_name.csv'))
 s50 <- vroom::vroom(here::here('output', region, 'ss50_ess_sz.csv')) %>% 
   mutate.(id = 50) 
 s75 <- vroom::vroom(here::here('output', region, 'ss75_ess_sz.csv')) %>% 
@@ -56,11 +56,23 @@ base <- vroom::vroom(here::here('output', region, 'base_ess_sz.csv')) %>%
 
 
 # length data 
-bind_rows.(s50, s75, s100, s125, s150, s175, base) %>% 
+bind_rows.(s50, s75, s100, s125, s150, s175) %>% 
   left_join.(spec) %>% 
-    mutate.(id = factor(id, c('50', '75', '100', '125', '150', '175', 'base'))) -> df
+    mutate.(id = factor(id, c('50', '75', '100', '125', '150', '175'))) -> df
 
 df<- df %>% filter(species!="Pacific ocean perch")
+
+samples <- factor(c('50', '75', '100', '125', '150', '175'))
+base_plots <- base %>%
+  left_join.(spec) %>% 
+  filter(species!="Pacific ocean perch") %>%
+  uncount(6) %>%
+  mutate(id = rep(samples, 54000)) %>%
+  tidytable::mutate.(ess = replace(ess, ess == "ess_f", "Female"),
+                     ess = replace(ess, ess == "ess_m", "Male"),
+                     ess = replace(ess, ess == "ess_t", "Total"))
+  
+  
 # plots ----
 
 #years 2017-2019
@@ -71,6 +83,7 @@ df %>% filter(year %in% 2017:2019) %>%
                      ess = replace(ess, ess == "ess_t", "Total")) %>% 
   ggplot(aes(id, value, fill = factor(ess))) + 
     geom_boxplot(outlier.size = 0, alpha = 0.7, outlier.shape = NA) +
+    geom_boxplot(data=base_plots, aes(fill=NULL, color=factor(ess)), outlier.size = 0, alpha = 0, outlier.shape = NA, size=.4) +
     coord_cartesian(ylim = c(0, 5000)) +
     facet_wrap(~species, scales = 'free') + 
     scico::scale_fill_scico_d("Sex", palette = 'roma') +
